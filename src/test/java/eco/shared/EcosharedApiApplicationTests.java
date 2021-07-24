caa5474fd4ebe35aa2095aff40dc2c0cef3136dc
Joao Guilherme de Souza <joao.souza@fcamara.com.br>
2021-07-24 11:01:32 -0300

    Project setup

---
+++ src/test/java/eco/shared/EcosharedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package eco.shared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcosharedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
